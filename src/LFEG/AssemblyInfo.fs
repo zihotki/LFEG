7359bd7af3ca6fd7d52a5d88a28aaa78ef80d21b
Vasili Puchko <vpuchko@suvoda.com>
2015-09-08 17:13:58 +0300

    initial commit

---
+++ src/LFEG/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("LFEG")>]
+[<assembly: AssemblyProductAttribute("LFEG")>]
+[<assembly: AssemblyDescriptionAttribute("LFEG - Lighting Fast Excel Generator, framework for efficient generation of excel files")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
